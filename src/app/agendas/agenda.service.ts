19030601d3c01b50f3b85d81066d3b74973ae550
Daniel Borquaye <danielborquaye21@gmail.com>
2017-06-24 14:29:29 +0100

    Agenda service logic

--- src/app/agendas/agenda.service.ts
+++ src/app/agendas/agenda.service.ts
@@ -1,16 +1,66 @@
 import { Injectable } from '@angular/core';
+import { Agenda } from 'app/agendas/agenda.model';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { Http } from '@angular/http/http';
 
 @Injectable()
 export class AgendaService {
+    private dataStore: {
+      agendas: Agenda[],
+      currentAgenda: Agenda
+  };
 
-  constructor() { }
+  private _agendas: BehaviorSubject<Agenda[]>;
+  public agendas: Observable<Agenda[]>;
 
-  addAgenda() {}
+  private _currentAgenda: BehaviorSubject<Agenda>;
+  public currentAgenda: Observable<Agenda>;
 
-  getAgenda() {}
 
-  editAgenda() {}
+  constructor(private _http: Http) {
+    this.dataStore = {
+      agendas: [
+        { id: 1, name: 'Recording transcripts'},
+        { id: 2, name: 'Follow-up meetings'},
+        { id: 3, name: 'Random stuff'}
+      ],
+      currentAgenda: null
+    }
 
-  removeAgenda() {}
+    this._agendas =  <BehaviorSubject<Agenda[]>>new BehaviorSubject([]);
+    this.agendas = this._agendas.asObservable();
+
+    this._currentAgenda =  <BehaviorSubject<Agenda>>new BehaviorSubject({});
+    this.currentAgenda = this._currentAgenda.asObservable();
+  }
+
+  getAgendas() {
+    // this._http.get(backendUrl, options)
+    //   .subscribe(result => {
+    //     this.dataStore.attendees = result;
+    //     this._attendees.next(Object.assign({}, this.dataStore).attendees);
+    //   })
+  }
+
+  addAgenda(agenda: Agenda) {
+    this.dataStore.agendas.push(agenda)
+    this._agendas.next(Object.assign({}, this.dataStore).agendas);
+  }
+
+  getAgenda(id: number) {
+    this.dataStore.currentAgenda = this.dataStore.agendas.find(agenda => agenda.id === id);
+    this._currentAgenda.next(Object.assign({}, this.dataStore).currentAgenda);
+  }
+
+  editAgenda(agenda: Agenda) {
+    this.dataStore.agendas.splice(agenda.id, 1, agenda)
+    this._agendas.next(Object.assign({}, this.dataStore).agendas);
+  }
+
+  removeAgenda(id: number) {
+    this.dataStore.agendas.splice(id, 1)
+    this._agendas.next(Object.assign({}, this.dataStore).agendas);
+  }
 
 }
