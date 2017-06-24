7e7e8d454a2045e7c4d0eed4c586c4cc65aa5205
Joseph Popoola <Mexcore@Josephs-MacBook-Pro.local>
2017-06-24 12:13:33 +0100

    did some bits still

---
+++ src/app/meeting/meeting.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MeetingService {
+
+  constructor() { }
+
+  addMeeting() {}
+
+  getMeeting() {}
+
+  editMeeting() {}
+
+  removeMeeting() {}
+}
