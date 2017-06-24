fbc71c8a5c4c35b5e972881a0dc3348caf80ef2f
Kiran Randhawa <info@primebydesign.co.uk>
2017-06-24 16:54:35 +0100

    Signalr implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SignalR, BroadcastEventListener } from "ng2-signalr";
 
 @Component({
   selector: 'orc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'orc';
+
+  constructor(private _signalR: SignalR) { 
+
+  }
+
+    ngOnInit() {
+
+    this._signalR.connect().then((c) => {
+
+      let onMessageSent = new BroadcastEventListener<any>('SpeechRecognised');
+      c.listen(onMessageSent)
+      onMessageSent.subscribe((msg: any) => {
+
+        let intent = msg.intent;
+        let entity = msg.entity;
+
+        console.log("intent: " + intent);
+        console.log("entity: " + entity);
+
+      });
+
+    });
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeetingComponent } from './meeting/meeting.component';
+import { SignalRModule, SignalRConfiguration } from "ng2-signalr";
+import 'signalr/jquery.signalR.js';
+
+// v2.0.0
+export function createConfig(): SignalRConfiguration {
+  const c = new SignalRConfiguration();
+  c.hubName = 'SignalrHub';
+  c.url = 'http://localhost:8080';
+  c.logging = true;
+  return c;
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +23,8 @@ import { MeetingComponent } from './meeting/meeting.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SignalRModule.forRoot(createConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
