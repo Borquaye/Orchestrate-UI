9953098521e076142498b6006cd93210df5f990a
Daniel Borquaye <danielborquaye21@gmail.com>
2017-06-24 13:11:29 +0100

    Methods for Attendee service

--- src/app/atendees/atendee.model.ts
+++ src/app/atendees/atendee.model.ts
@@ -1,3 +1,4 @@
 export class Attendee {
+    id: number;
     name: string;
 }
--- src/app/atendees/atendee.service.ts
+++ src/app/atendees/atendee.service.ts
@@ -1,16 +1,75 @@
 import { Injectable } from '@angular/core';
+import { Attendee } from 'app/atendees/atendee.model';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class AtendeeService {
+  private dataStore: {
+    attendees: Attendee[],
+    currentAttendee: Attendee
+  };
 
-  constructor() { }
+  private _attendees: BehaviorSubject<Attendee[]>;
+  public attendees: Observable<Attendee[]>;
 
-  addAtendee() {}
+  private _currentAttendee: BehaviorSubject<Attendee>;
+  public currentAttendee: Observable<Attendee>;
 
-  getAtendee() {}
 
-  editAtendee() {}
 
-  removeAtendee() {}
+  constructor(private _http: Http) {
+    this.dataStore = {
+      attendees: [
+        { id: 1, name: 'Daniel'},
+        { id: 2, name: 'Joe'},
+        { id: 3, name: 'Sam'}
+      ],
+      currentAttendee: null
+    }
+
+    this._attendees =  <BehaviorSubject<Attendee[]>>new BehaviorSubject([]);
+    this.attendees = this._attendees.asObservable();
+
+    this._currentAttendee =  <BehaviorSubject<Attendee>>new BehaviorSubject({});
+    this.currentAttendee = this._currentAttendee.asObservable();
+
+  }
+
+  getAttendees() {
+    // this._http.get(backendUrl, options)
+    //   .subscribe(result => {
+    //     this.dataStore.attendees = result;
+    //     this._attendees.next(Object.assign({}, this.dataStore).attendees);
+    //   })
+  }
+
+  addAttendee(attendee: Attendee) {
+    // this._http.get(backendUrl, options)
+    //   .subscribe(result => {
+    //     // If result is successfull
+    //     this.dataStore.attendees.push(attendee)
+    //     this._attendees.next(Object.assign({}, this.dataStore).attendees);
+    //   })
+
+    this.dataStore.attendees.push(attendee)
+    this._attendees.next(Object.assign({}, this.dataStore).attendees);
+  }
+
+  getAttendee(id: number) {
+    this.dataStore.currentAttendee = this.dataStore.attendees.find(attendee => attendee.id === id);
+    this._currentAttendee.next(Object.assign({}, this.dataStore).currentAttendee);
+  }
+
+  editAttendee(attendee: Attendee) {
+    this.dataStore.attendees.splice(attendee.id, 1, attendee)
+    this._attendees.next(Object.assign({}, this.dataStore).attendees);
+  }
+
+  removeAttendee(id: number) {
+    this.dataStore.attendees.splice(id, 1)
+    this._attendees.next(Object.assign({}, this.dataStore).attendees);
+  }
 
 }
