ffced42ad0751bc5f86c22e3920534bc74980649
Daniel Borquaye <danielborquaye21@gmail.com>
2017-06-24 19:21:35 +0100

    dam errors

--- src/app/actions/action.service.ts
+++ src/app/actions/action.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
-import {Action} from "app/actions/action.model";
-import {BehaviorSubject} from "rxjs/BehaviorSubject";
-import {Observable} from "rxjs/Observable";
-import {Http} from "@angular/http";
-import {ACTION_STATUS} from "app/shared/action-status.enum";
+import {Action} from 'app/actions/action.model';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Observable} from 'rxjs/Observable';
+import {ACTION_STATUS} from 'app/shared/action-status.enum';
 import {AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2/database';
 
 @Injectable()
 export class ActionService {
   actionItems: FirebaseListObservable<any[]>;
-  action: FirebaseObjectObservable<any>;  
+  action: FirebaseObjectObservable<any>;
 
   constructor(private db: AngularFireDatabase) {
     this.actionItems = db.list('https://meetinganalasys.firebaseio.com/new');
